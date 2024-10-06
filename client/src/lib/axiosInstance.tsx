871c9ef428851eef917239f2faad2c14ce3e8297
Sid Björk <sidricobjork@gmail.com>
2024-10-06 12:23:47 +0200

    add

--- client/src/lib/axiosInstance.tsx
+++ client/src/lib/axiosInstance.tsx
@@ -14,7 +14,7 @@ export default axiosInstance; */
 /* This is for production */
 
 const axiosInstance = axios.create({
-    baseURL: 'https://allcode-community-project-z4em.vercel.app',
+    baseURL: 'https://allcode-community-project-z4em.vercel.app:8080',
     headers: {
         'x-access-token': localStorage.getItem("token"),
     },
--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5050");
+const socket = io("https://allcode-community-project-z4em.vercel.app:5050");
 
 export default socket
