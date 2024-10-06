f7320e66023144e20d86331ad342263ee7c153ea
Sid Björk <sidricobjork@gmail.com>
2024-10-06 14:12:06 +0200

    add

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -2,7 +2,8 @@ import { io } from "socket.io-client";
 
 const socket = io("https://allcode-community-project-z4em.vercel.app", {
   path: "/api/socket",
-  transports: ['websocket']
+  transports: ['websocket'],
+  withCredentials: true
 });
 
 export default socket;
