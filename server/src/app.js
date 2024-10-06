43f5a5d38a12cf189222a61df66f6a4f20ce1ddf
Sid Björk <sidricobjork@gmail.com>
2024-10-06 12:54:41 +0200

    add

--- server/src/app.js
+++ server/src/app.js
@@ -15,4 +15,11 @@ app.get('/', (req, res) => {
     res.json({ message: "Hello World" });
 });
 
+// Handle 404 - Keep this as the last route
+app.use((req, res, next) => {
+    res.status(404).send("Sorry, that route doesn't exist.");
+});
+
 export default app;
+
+// For local developmen
