c26ba9bdc8a4af6119385a801a8f37c16a94a1fd
Sid Björk <sidricobjork@gmail.com>
2024-10-08 16:32:02 +0200

    add

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://socket.allcodecommunity.com:7070");
+const socket = io("https://socket.allcodecommunity.com");
 
 export default socket;
