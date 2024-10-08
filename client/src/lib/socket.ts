d061ef9a9905dd27f289badaf7f6fcc7fde74da1
Sid Björk <sidricobjork@gmail.com>
2024-10-08 14:18:39 +0200

    add

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,11 +1,5 @@
 import { io } from "socket.io-client";
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-const SOCKET_URL = isProduction
-  ? "https://allcode-community-project-z4em.vercel.app"
-  : "http://localhost:7070";
-
-const socket = io(SOCKET_URL);
+const socket = io("https://socket.allcodecommunity.com:7070");
 
 export default socket;
