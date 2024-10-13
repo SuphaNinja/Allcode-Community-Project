2d727d13a73711eca94db22a2f8b3b60b8313fb1
Sid Björk <sidricobjork@gmail.com>
2024-10-13 12:15:26 +0200

    add

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const isLocal = process.env.NODE_ENV === "development";
 
-const socket = io(isLocal ? "http://localhost:7070" : "https://socket.allcodecommunity.com", {
+const socket = io(isLocal ? "http://localhost:7070" : "wss://socket.allcodecommunity.com", {
   withCredentials: true,
   transports: ['websocket', 'polling']
 });
