962f706fdfdcb7a1fad230a1addd5f52aaac46c4
Sid Björk <sidricobjork@gmail.com>
2024-10-13 12:29:04 +0200

    add

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -2,9 +2,14 @@ import { io } from "socket.io-client";
 
 const isLocal = process.env.NODE_ENV === "development";
 
+const token = localStorage.getItem("token")
+
 const socket = io(isLocal ? "http://localhost:7070" : "https://socket.allcodecommunity.com", {
   withCredentials: true,
-  transports: ['websocket', 'polling']
+  transports: ['websocket', 'polling'],
+  extraHeaders: {
+    "x-access-token": token ? token : "123"
+  }
 });
 
 export default socket;
